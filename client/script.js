bc97c3315f4756e3cf2d9276d1be97a59390b8e5
Fangwei Jiang <emberspirit2018@gmail.com>
2023-08-06 03:51:28 +1000

    upload code

---
+++ client/script.js
@@ -0,0 +1,100 @@
+// 删除msg
+async function handleDelete() {
+    try {
+        const judgeDelete = prompt(`Hi ${this.previousElementSibling.previousElementSibling.textContent}! Are you sure to delete "${this.previousElementSibling.textContent}" ? Y/N`);
+        if (!(judgeDelete.toLowerCase() === "y")) return;
+        const response = await axios.delete(`http://localhost:8080/messages/${this.getAttribute("data-id")}`);
+        response.status === 201 ? getMessages() : null;
+    } catch (error) {
+        console.error("Error deleting message:", error);
+    }
+}
+
+//更新msg
+async function handleUpdate() {
+    document.querySelectorAll("li button:last-of-type").forEach(item => {
+        item.disabled = true;
+        item.className = "button_disabled";
+    });
+    const field = document.createElement("section");
+    const message_list = document.getElementById("message-list");
+    const name = this.closest("li").querySelector("b").textContent;
+    field.innerHTML = `
+    <label for='comment'>Hi <b>${name}</b>! Please update your comment</label>
+    <input type='text' id='comment' name='comment' placeholder='Comment'/>
+    <button class="update-button">Finish edit</button>`;
+    message_list.appendChild(field);
+    field.querySelector(".update-button").addEventListener("click", async () => {
+        const comment = field.querySelector("#comment").value;
+        if (!comment) {
+            alert(`Hi ${name}! Your updated comment is empty`);
+            return;
+        }
+        try {
+            const response = await axios.put(
+                `http://localhost:8080/messages/${this.getAttribute("data-id")}`,
+                { name: name, message: comment },
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+            message_list.removeChild(field);
+            response.status === 201 ? getMessages() : null;
+        } catch (error) {
+            console.error("Error updating message:", error);
+        }
+    });
+}
+
+// display messages
+async function getMessages() {
+    try {
+        const response = await axios.get("http://localhost:8080/messages");
+        const messages = response.data;
+        const messageList = document.getElementById("message-list");
+        messageList.innerHTML = "";
+        messages.forEach(message => {
+            const messageItem = document.createElement("li");
+            messageItem.innerHTML = `
+            <b>${message.name}</b>
+            <span>${message.message}</span>
+            <button class="delete-button" data-id="${message.id}">Delete</button>
+            <button class="update-button" data-id="${message.id}">Update</button>`;
+            messageList.appendChild(messageItem);
+        });
+        document.querySelectorAll(".delete-button").forEach(item => item.addEventListener("click", handleDelete));
+        document.querySelectorAll(".update-button").forEach(item => item.addEventListener("click", handleUpdate));
+    } catch (error) {
+        console.error("Error displaying message:", error);
+    }
+}
+
+// post message
+async function handleSubmitMessage(e) {
+    e.preventDefault();
+    try {
+        const response = await axios.post(
+            "http://localhost:8080/messages",
+            {
+                name: this.children[0].value,
+                message: this.children[1].value,
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+        if (response.status === 201) {
+            getMessages();
+            this.children[0].value = "";
+            this.children[1].value = "";
+        }
+    } catch (error) {
+        console.error("Error occurred while submitting message:", error);
+    }
+}
+getMessages();
+document.getElementById("message-form").addEventListener("submit", handleSubmitMessage);
---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+let messages = [{ id: 1, name: "John", message: "I love programming!" }];
+
+function getMessageById(messageId) {
+    return messages.find(msg => msg.id === messageId);
+}
+
+function handleErrorMessage(res, messageId) {
+    res.status(404).json({ msg: `id ${messageId} does not exist` });
+}
+
+function handleParameters(req, res) {
+    if (Object.keys(req.body).length !== 2) {
+        res.status(400).json({ msg: "Invalid request body. Only 'name' and 'message' properties are allowed." });
+        return false;
+    } else if (!req.body.name || !req.body.message) {
+        res.status(400).json({ msg: "name or message is empty" });
+        return false;
+    } else return true;
+}
+
+//GET:获取所有留言
+app.get("/messages", (req, res) => {
+    console.log("current message array", messages);
+    res.status(200).json(messages);
+});
+
+//GET:获得单一留言
+app.get("/messages/:id", (req, res) => {
+    const messageId = parseInt(req.params.id);
+    const message = getMessageById(messageId);
+    message ? res.status(200).json(message) : handleErrorMessage(res, messageId);
+});
+
+//POST: 添加留言
+app.post("/messages", (req, res) => {
+    if (!handleParameters(req, res)) {
+        return;
+    }
+    const newMessage = {
+        id: messages.length + 1,
+        name: req.body.name,
+        message: req.body.message,
+    };
+    messages.push(newMessage);
+    console.log("newMessage", newMessage);
+    res.status(201).json({ msg: "create successfully", newData: newMessage });
+});
+
+//DELETE:删除留言
+app.delete("/messages/:id", (req, res) => {
+    const messageId = parseInt(req.params.id);
+    const messageIndex = messages.findIndex(msg => msg.id === messageId);
+    if (messageIndex !== -1) {
+        if (messageIndex !== messages.length - 1) for (let i = messageIndex + 1; i < messages.length; i++) --messages[i].id;
+        const deletedMessage = messages.splice(messageIndex, 1)[0];
+        console.log("deletedMessage", deletedMessage);
+        res.status(201).json({ msg: "delete successfully", deleteData: deletedMessage });
+    } else handleErrorMessage(res, messageId);
+});
+
+//Update:更新留言
+app.put("/messages/:id", (req, res) => {
+    if (!handleParameters(req, res)) {
+        return;
+    }
+    const messageId = parseInt(req.params.id);
+    const messageToUpdate = getMessageById(messageId);
+    if (messageToUpdate) {
+        Object.assign(messageToUpdate, req.body);
+        console.log("updateMessage", messageToUpdate);
+        res.status(201).json({ msg: "update successfully", updateData: messageToUpdate });
+    } else handleErrorMessage(res, messageId);
+});
+
+app.listen(8080, () => console.log("server is sunning on port 8080"));
